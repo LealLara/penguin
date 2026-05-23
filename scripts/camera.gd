082acd81007b85c07b0fba90e4fb162e67eaba59
LealLara <motionverso0@gmail.com>
2026-05-23 14:41:25 -0300

    map

--- scripts/camera.gd
+++ scripts/camera.gd
@@ -1,11 +1,22 @@
 extends Camera2D
 
-
+var target: Node2D
+ 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	pass # Replace with function body.
+		get_target()
+	 
 
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
-	pass
+	position = target.position
+	
+	
+	
+func get_target():
+	var nodes = get_tree().get_nodes_in_group("player")
+	if nodes.size() == 0:
+		push_error("player not found")
+		return
+		 
+	target = nodes[0]
+	
