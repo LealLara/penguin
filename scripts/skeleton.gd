c87d353502024d16286d96a85bdcf10a2418346b
LealLara <motionverso0@gmail.com>
2026-07-06 11:06:24 -0300

    attack

--- scripts/skeleton.gd
+++ scripts/skeleton.gd
@@ -5,19 +5,22 @@ enum SkeletonState{
 	attack,
 	hurt
 }
-@onready var colli : CollisionShape2D = $CollisionShape2D 
+const SPINNING_BONE = preload("uid://b5y3wut3rrwoe") 
 
+@onready var colli : CollisionShape2D = $CollisionShape2D 
 @onready var anim: AnimatedSprite2D = $AnimatedSprite2D
 @onready var hitbox: Area2D = $Hitbox
 @onready var wall_detector: RayCast2D = $WallDetector
 @onready var ground_detector: RayCast2D = $GroundDetector
 @onready var player_detector: RayCast2D = $PlayerDetector
+@onready var bone_start_position: Node2D = $BoneStartPosition
 
- 
+
 const SPEED = 10.0
 const JUMP_VELOCITY = -400.0
-var direction = 1
 var status: SkeletonState
+var direction = 1
+var can_throw = true
 
 func _ready() -> void:
 	go_to_walk_state()
@@ -45,6 +48,7 @@ func go_to_attack_state():
 	status = SkeletonState.attack
 	anim.play("attack")
 	velocity = Vector2.ZERO
+	can_throw = true
 	
 func go_to_hurt_state():
 	status = SkeletonState.hurt
@@ -75,7 +79,9 @@ func walk_state(_delta):
 		return
 	
 func attack_state(_delta):
-	pass
+	if anim.frame == 2 && can_throw:
+		throw_bone()
+		can_throw = false
 
 func hurt_state(_delta):
 	pass
@@ -100,3 +106,10 @@ func _on_animated_sprite_2d_animation_finished() -> void:
 	if anim.animation ==  "attack":
 		go_to_walk_state()
 		return
+
+func throw_bone():
+	var new_bone = SPINNING_BONE.instantiate()
+	add_sibling(new_bone)
+	new_bone.position = bone_start_position.global_position
+	new_bone.set_direction(self.direction)
+	
